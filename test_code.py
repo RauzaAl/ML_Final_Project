cedf8850eb0817339d69711b0812bebf68e5c3fe
Alshymbaeva Rauza <40315161+RauzaAl@users.noreply.github.com>
2020-04-28 22:17:18 +0600

    Update test_code.py

--- test_code.py
+++ test_code.py
@@ -3,22 +3,19 @@ import cv2
 import pickle
 import tkinter
 
-########### PARAMETERS ##############
 width = 640
 height = 480
 threshold = 0.75
-#####################################
+
 print("Hello world")
-#### CREATE CAMERA OBJECT
+
 cap = cv2.VideoCapture(0)
 cap.set(3,width)
 cap.set(4,height)
 
-#### LOAD THE TRAINNED MODEL
 pickle_in = open("model_trained.p","rb")
 model = pickle.load(pickle_in)
 
-#### PREPORCESSING FUNCTION
 def preProcessing(img):
     img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     img = cv2.equalizeHist(img)
@@ -77,11 +74,10 @@ while True:
     img = preProcessing(img)
     cv2.imshow("Processsed Image",img)
     img = img.reshape(1,32,32,1)
-    #### PREDICT
+
     classIndex = int(model.predict_classes(img))
     print(classIndex)
     predictions = model.predict(img)
-    #print(predictions)
     probVal= np.amax(predictions)
     print(classIndex,probVal)
 
